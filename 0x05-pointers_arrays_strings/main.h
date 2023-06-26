2aa8359e46743acdd501132e3c9e5ee610a22c98
joye21 <yosephwossenu21@gmail.com>
2023-06-26 13:15:20 -0700

    0-rest_to98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+  2 #define MAIN_H
+  3
+  4 int _putchar(char c);
+  5
+  6 void reset_to_98(int *n);
+  7 void swap_int(int *a, int *b);
+  8 int _strlen(char *s);
+  9 void _puts(char *str);
+ 10 void print_rev(char *s);
+ 11 void rev_string(char *s);
+ 12 void puts2(char *str);
+ 13 void puts_half(char *str);
+ 14 void print_array(int *a, int n);
+ 15 char *_strcpy(char *dest, char *src);
+ 16
+ 17 #endif /* MAIN_H */
